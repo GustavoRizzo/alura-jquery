53a45ee3f0d5b0981dcecfb0732d5e84ecde9dc6
Eduardo de Albuquerque <erizzoalbuquerque@gmail.com>
2019-12-28 19:29:34 -0300

    aula 4 completada

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,63 @@
-console.log("olá mundo");
-var frase = $(".frase").text();
-numPalavras = frase.split(" ").length;
-console.log(numPalavras);
-
-var tamanhoFrase =$("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = 5;
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
+$("#botao-reiniciar").click(reiniciaJogo);
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
+$(function(){
+    console.log("página carregou!");
 
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+    
+
+    atualizaTamanhoFrase();
+    iniciaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = 5;
-$("#tempo-digitacao").text(tempoRestante);
 
-campo.one("focus", function() {
-    console.log("focado");
-    var cronometroID = setInterval(function() {
-        console.log("loppando");
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1){
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
-    },1000);
-});
+function atualizaTamanhoFrase (){
+    var frase = $(".frase").text();
+    numPalavras = frase.split(" ").length;
+    console.log(numPalavras);
+
+    var tamanhoFrase =$("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function iniciaContadores (){
+    campo.on("input", function() {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro (){
+    var tempoRestante = 5;
+    $("#tempo-digitacao").text(tempoRestante);
+    campo.one("focus", function() {
+        console.log("focado");
+        var cronometroID = setInterval(function() {
+            console.log("loppando");
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1){
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        },1000);
+    });
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled",false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
+
